48d391308e284c6a92529205c135b9afab9b3e2c
tvs111 <73558991+tkachenko0@users.noreply.github.com>
2023-07-04 09:36:15 +0200

    ref

--- custom_libs/dump.py
+++ custom_libs/dump.py
@@ -1,19 +1,29 @@
 import joblib
+import os
 
-extention = 'pkl'
+extension = 'pkl'
+folder = '../dump_models'
 
 
 def save_vectorizer(vectorizer, filename):
-    joblib.dump(vectorizer, f'{filename}.{extention}')
+    filepath = os.path.join(folder, f'{filename}.{extension}')
+    print("Saving vectorizer at:", filepath)
+    joblib.dump(vectorizer, filepath)
 
 
 def load_vectorizer(filename):
-    return joblib.load(f'{filename}.{extention}')
+    filepath = os.path.join(folder, f'{filename}.{extension}')
+    print("Loading vectorizer from:", filepath)
+    return joblib.load(filepath)
 
 
 def save_model(model, filename):
-    joblib.dump(model, f'{filename}.{extention}')
+    filepath = os.path.join(folder, f'{filename}.{extension}')
+    print("Saving model at:", filepath)
+    joblib.dump(model, filepath)
 
 
 def load_model(filename):
-    return joblib.load(f'{filename}.{extention}')
+    filepath = os.path.join(folder, f'{filename}.{extension}')
+    print("Loading model from:", filepath)
+    return joblib.load(filepath)
