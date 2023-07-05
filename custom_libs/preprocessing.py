410fdfcddaf7195c4375446262c6934a8f40753f
tvs111 <73558991+tkachenko0@users.noreply.github.com>
2023-07-05 21:40:00 +0200

    init

--- custom_libs/preprocessing.py
+++ custom_libs/preprocessing.py
@@ -4,6 +4,17 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 
+def preprocess_dataframe(df, column_name, proprocessing_function):
+    df[column_name+"_clean"] = df[column_name].apply(proprocessing_function)
+
+
+def add_id_column(df, columns):
+    df.columns = df.columns.str.strip()
+    df['id'] = df.groupby(columns).ngroup()
+    num = df['id'].nunique()
+    print("Number of unique ids: ", num)
+
+
 def preprocess_text1(raw_review):
     review_text = BeautifulSoup(
         raw_review, 'html.parser').get_text()  # delete html
